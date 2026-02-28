93a9044eadfa91b71b6e7bf509434157c8cd1736
Yudistiro Septian Dwi Saputro <yudistiro6@gmail.com>
2026-02-28 11:23:06 +0700

    fix :implement app database

--- data/src/main/kotlin/com/blank/data/local/AppDatabase.kt
+++ data/src/main/kotlin/com/blank/data/local/AppDatabase.kt
@@ -6,11 +6,10 @@ import com.blank.data.local.dao.ArticleDao
 import com.blank.data.local.entity.ArticleEntity
 
 @Database(
-    entities = [ArticleEntity::class, RemoteKeyEntity::class],
-    version = 2,
+    entities = [ArticleEntity::class],
+    version = 3,
     exportSchema = true,
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun articleDao(): ArticleDao
-    abstract fun remoteKeyDao(): RemoteKeyDao
 }
